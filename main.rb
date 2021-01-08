1fff7444276b3662566f57ad6ce056d1c373d315
Delton Ding <dsh0416@gmail.com>
2021-01-09 04:06:19 +0800

    Fix flight plan

--- main.rb
+++ main.rb
@@ -13,19 +13,19 @@ KRPC.connect do |client|
   vessel = client.space_center.active_vessel
 
   # VR: 100m/s, Climb at 390 knots to 2000m
-  # TakeoffProcess.new(vessel, 100, 200, 2000).run
-  # # Upwind
-  # WaypointProcess.new(vessel, 200, 2000, 0.0, -70.0).run
-  # # Crosswind
-  # WaypointProcess.new(vessel, 200, 2000, -5.0, -70.0).run
-  # # Downwind
-  # WaypointProcess.new(vessel, 200, 1500, -5.0, -77.7667).run
-  # # Base
-  # WaypointProcess.new(vessel, 170, 1000, -0.04833333, -77.7667).run
-  # # Final
-  # WaypointProcess.new(vessel, 120, 1000, -0.04833333, -76).run
-  # # Approach
-  # ApproachProcess.new(vessel, 100, 80, -0.04833333, -74.72389).run
+  TakeoffProcess.new(vessel, 100, 200, 2000).run
+  # Upwind
+  WaypointProcess.new(vessel, 200, 2000, 0.0, -70.0).run
+  # Crosswind
+  WaypointProcess.new(vessel, 200, 2000, -5.0, -70.0).run
+  # Downwind
+  WaypointProcess.new(vessel, 200, 1500, -5.0, -77.7667).run
+  # Base
+  WaypointProcess.new(vessel, 170, 1000, -0.04833333, -77.7667).run
+  # Final
+  WaypointProcess.new(vessel, 120, 1000, -0.04833333, -76).run
+  # Approach
+  ApproachProcess.new(vessel, 100, 80, -0.04833333, -74.72389).run
   # Land
   LandProcess.new(vessel, -0.04833333, -73.5).run
 end
